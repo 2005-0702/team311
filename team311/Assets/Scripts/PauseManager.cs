644d41268a325a36c3dc22211ce0f1295cd5baa5
syundayo1019-ctrl <syundayo.1019@gmail.com>
2026-04-26 12:01:08 +0900

    Escキーでポーズ画面表示、仮ボタン配置

---
+++ team311/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    
+
+}
