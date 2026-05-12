e509897b92f4a1bae1983446b94a4c0a75c33cd7
mori-kawa <math.1125.yngnks@gmail.com>
2026-05-12 17:33:16 +0900

    とげの追加。プレイヤーは当たると即死。

---
+++ team311/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 1; // 最大体力
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // ダメージを受ける命令（トゲから呼ばれる）
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("痛い！ 残り体力: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("ゲームオーバー！ゲームを終了します。");
+
+        // 1. Unityエディタ上での実行を停止する
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // 2. 実際にアプリとして書き出した（ビルドした）後にアプリを終了する
+            Application.Quit();
+#endif
+    }
+}
